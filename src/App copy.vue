778c857374b392d751dcd6a15446a5c3302b2674
hiual <2897687312@qq.com>
2022-11-30 19:40:42 +0800

    项目初始化

---
+++ src/App copy.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>App
+    <p>{{ $store.state.username }}</p>
+    <p>{{ $store.getters['newName'] }}</p>
+    <button @click="mutationsFn">mutationsFn</button>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'App',
+  setup () {
+    const store = useStore()
+    console.log(store.state.username)
+    console.log(store.getters.newName)
+    // store.commit('updateName')
+    const mutationsFn = () => {
+      // store.commit('updateName')
+      store.dispatch('updateName')
+    }
+    return { mutationsFn }
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,34 @@
 import { createStore } from 'vuex'
 
+// 创建vuex仓库并导出
+// vue2.0 创建仓库 new Vuex.Store({})
+// vue3.0 创建仓库 createStore({})
 export default createStore({
   state: {
+    // 数据
+    // username: 'zs'
   },
   getters: {
+    // vuex的计算属性
+    // newName(state) {
+    //     return state.username + '????'
+    // }
   },
   mutations: {
+    // 改数据函数
+    // updateName(state) {
+    //     state.username = 'ls'
+    // }
   },
   actions: {
+    // 请求数据函数
+    // updateName(ctx) {
+    //     setTimeout(() => {
+    //         ctx.commit('updateName')
+    //     }, 1000)
+    // }
   },
   modules: {
+    // 分模块
   }
 })
